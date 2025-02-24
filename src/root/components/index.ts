92da276fe00f19dcc5d014f4bea9049cffbebdc5
Rustam <rustamzaloldinov@gmail.com>
2025-02-24 20:54:12 +0200

    Added: New imports

--- src/root/components/index.ts
+++ src/root/components/index.ts
@@ -1,5 +1,7 @@
 import { PriceTable } from "@/root/components/PriceTable/PriceTable";
 import { Table } from "@/root/components/PriceTable/components/Table/Table";
 import { FilterSortControls } from "@/root/components/FilterSortControls/FilterSortControls";
+import { BookingForm } from "@/root/components/BookingForm/BookingForm";
+import { PlanetFilter } from "@/root/components/PlanetFilter/PlanetFilter";
 
-export { PriceTable, Table, FilterSortControls };
+export { PriceTable, Table, FilterSortControls, BookingForm, PlanetFilter };
--- src/root/hooks/index.ts
+++ src/root/hooks/index.ts
@@ -1,5 +1,11 @@
 import { useFetchRoutes } from "@/root/hooks/useFetchRoutes/useFetchRoutes";
 import { useFilterSortRoutes } from "@/root/hooks/useFilterSortRoutes/useFilterSortRoutes";
 import { useCalculateTravelDetails } from "@/root/hooks/useCalculateTravelDetails/useCalculateTravelDetails";
+import { useGenerateAndFilterRoutes } from "@/root/hooks/useGenerateRoutes/useGenerateRoutes";
 
-export { useFetchRoutes, useFilterSortRoutes, useCalculateTravelDetails };
+export {
+  useFetchRoutes,
+  useFilterSortRoutes,
+  useCalculateTravelDetails,
+  useGenerateAndFilterRoutes,
+};
